67c279024d9329d9c455dca7b702d95073e42bb3
Lepidophylla <ericroberto2322@gmail.com>
2023-06-03 00:21:12 +0700

    balekin resep

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -8,13 +8,30 @@ use Illuminate\Support\Facades\Validator;
 
 class RecipeController extends Controller
 {
-    
-    public function index(){
-        $recipe = Recipe::all();
+    public function index(Request $request){
 
+        $data = Validator::make($request->all(), [
+            'page' => 'required|integer',
+            'size' => 'required|integer'
+        ]);
+
+        if($data->fails()){
+            return response([
+                'message' => 'Failed',
+                'data' => $data->errors()
+            ], 400);
+        }
+
+        $page = $request->page;
+        $size = $request->size;
+
+        $recipe = recipe::skip(($page-1)*$size)->take($size)->get();
+        
+        // $recipe = recipe::where('name','=','Sauteed Bananas with Cardamom Praline Sauce')->get();
+        
         if(count($recipe) > 0){
             return response([
-    
+                'message' => 'Retrieve All Success',
                 'data' => $recipe
             ], 200);
         }
@@ -22,7 +39,7 @@ class RecipeController extends Controller
         return response([
             'message' => 'Empty',
             'data' => null
-        ], 400); 
+        ], 400);
     }
     
 }
