79ddd4bb10eaf9e317ed5f99d776670b77179221
Lepidophylla <ericroberto2322@gmail.com>
2023-06-11 04:03:46 +0700

    add BMI

--- app/Http/Controllers/dataUserController.php
+++ app/Http/Controllers/dataUserController.php
@@ -49,6 +49,7 @@ class dataUserController extends Controller
         $dataUser->height = $request->input('height');
         // $dataUser->bmr = $request->input('bmr');
         $dataUser->gender = $request->input('gender');
+        $dataUser->bmi = $request->input('weight')/(($request->input('height')/100)*($request->input('height')/100));
         $dataUser->user_id = $user;
 
         $activityLevel = $request->input('activity_level');
